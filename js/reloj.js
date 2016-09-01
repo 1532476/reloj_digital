af92f9c9cd3336b79ddf5b738f06885fef50d695
1532476 <1532476@utp.edu.pe>
2016-09-01 21:35:02 +0800

    primera_version
    subido desde la git hub de escritorio.

---
+++ js/reloj.js
@@ -0,0 +1,71 @@
+(function(){
+	var actualizarHora = function(){
+		var fecha = new Date(),
+			horas = fecha.getHours(),
+			ampm,
+			minutos = fecha.getMinutes(),
+			segundos = fecha.getSeconds(),
+			diaSemana = fecha.getDay(),
+			dia = fecha.getDate(),
+			mes = fecha.getMonth(),
+			year = fecha.getFullYear();
+
+		var pHoras = document.getElementById('horas'),
+			pAMPM = document.getElementById('ampm'),
+			pMinutos = document.getElementById('minutos'),
+			pSegundos = document.getElementById('segundos'),
+			pDiaSemana = document.getElementById('diaSemana'),
+			pDia = document.getElementById('dia'),
+			pMes = document.getElementById('mes'),
+			pYear = document.getElementById('year');
+
+		/* Agregando Contenido al div fecha  */
+
+		var semana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+		//textContent sirve para meter conenido dentro de 1 elemento
+		pDiaSemana.textContent = semana[diaSemana];
+		pDia.textContent = dia;
+		var meses = ['enero', 'febrero', 'marzo', 'abril', 'marzo', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre','noviembre', 'diciembre']; 
+		pMes.textContent = meses[mes]; 
+		pYear.textContent = year; 
+
+		/* Agregando Contenido al div reloj  */
+
+				
+
+		/*if (horas<12) {
+			ampm = "AM"
+		} 
+		else { ampm = "PM"
+
+		}*/
+
+		if (horas >= 12) {
+			horas = horas - 12;
+			ampm = "PM";
+		}	else {
+			ampm = "AM";
+		}
+		
+		if (horas == 0) {
+			horas =  12;
+		}
+
+		pHoras.textContent = horas;
+		pAMPM.textContent = ampm; 
+
+		if (minutos < 10) {
+			minutos = "0"+minutos;
+		}
+
+		if (segundos < 10) {
+			segundos = "0"+segundos;
+		}
+
+		pMinutos.textContent = minutos;
+		pSegundos.textContent = segundos;
+	};
+	actualizarHora();
+
+	var intervalo = setInterval(actualizarHora, 1000);
+}())
